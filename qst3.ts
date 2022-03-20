3a5ee69372b304c44bc8e728671f75b140270e73
I09 <76914817+I09-ben@users.noreply.github.com>
2022-03-21 00:13:15 +0100

    Add files via upload

---
+++ qst3.ts
@@ -0,0 +1,6 @@
+cache-controle":"no-cache"
+cache-controle:contient des directives pour la mise en cache tant dans les requetes que dans 
+les reponse .une directive donnée dans une requete ne signifie pas que la meme directive  doit 
+se trouver dans la réponse 
+no-cache:indique de renvoyer systématiquement la requete au serveur et ne sevir une eventuelle 
+version en cache que dans le cas ou le serveur le demande
